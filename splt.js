2eb332183556c8585f21547334feb6eec861f2f7
lauirf <592184299@qq.com>
2025-01-09 23:13:01 +0800

    add delay

--- splt.js
+++ splt.js
@@ -6,7 +6,11 @@ Splatoon3.options.cache = {
    enabled: true,
    ttl: 60,
 };
-var res = await Splatoon3.getSalmonRun();
-var weapons = res.regularSchedules[0].weapons;
-var array = [weapons[0].name,weapons[1].name,weapons[2].name,weapons[3].name]
-console.log(JSON.stringify(array));
+
+setTimeout(async () => {
+   var res = await Splatoon3.getSalmonRun();
+   var weapons = res.regularSchedules[0].weapons;
+   var array = [weapons[0].name, weapons[1].name, weapons[2].name, weapons[3].name]
+   console.log(JSON.stringify(array));
+}, 2000); // 延时 2000 毫秒 (2 秒)
+
