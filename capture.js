d4383a54cca3a5d30cc93e837a20ead4a5c849a0
lauirf <592184299@qq.com>
2025-01-09 22:36:51 +0800

    base issue

--- capture.js
+++ capture.js
@@ -11,7 +11,9 @@ import path from 'path'; // 用于处理文件路径
   const filePath = path.resolve('output/salmonRun.html');
 
   // 加载 HTML 文件
-  await page.goto(`file://${filePath}`);
+  const response = await page.goto(`file://${filePath}`);
+
+  await page.setContent((await response.buffer()).toString('utf8'));
 
   // 设置截图尺寸
   await page.setViewport({
