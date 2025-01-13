b64061cdc024ad3ee5d1f58d343002cde24a7b55
lauirf <592184299@qq.com>
2025-01-13 16:32:53 +0800

    add stage

--- splt.js
+++ splt.js
@@ -7,10 +7,7 @@ Splatoon3.options.cache = {
    ttl: 60,
 };
 
-setTimeout(async () => {
-   var res = await Splatoon3.getSalmonRun();
-   var weapons = res.regularSchedules[0].weapons;
-   var array = [weapons[0].name, weapons[1].name, weapons[2].name, weapons[3].name]
-   console.log(JSON.stringify(array));
-}, 2000); // 延时 2000 毫秒 (2 秒)
-
+var res = await Splatoon3.getSalmonRun();
+var weapons = res.regularSchedules[0].weapons;
+var array = [weapons[0].name, weapons[1].name, weapons[2].name, weapons[3].name]
+console.log(JSON.stringify(array));
